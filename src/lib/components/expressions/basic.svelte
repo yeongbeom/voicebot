606b06a4597208cac6ed90a3a241cde930cf37ba
comverser <hyunduk0206@gmail.com>
2022-03-25 07:06:30 +0000

    -

--- src/lib/components/expressions/basic.svelte
+++ src/lib/components/expressions/basic.svelte
@@ -402,11 +402,14 @@
 	function watchFunction() {
 		morph(expressionPaths[$expression]);
 	}
-	const handleClick = (e) => {
+	const changeExpression = (e) => {
 		const expr = e.target.innerText.toLowerCase();
 		emotion = expr;
-		talk = true;
 		$expression = expr;
+	};
+
+	const toggleTalkStatus = () => {
+		talk = !talk;
 	};
 
 	onMount(() => {
@@ -415,6 +418,7 @@
 		show = true;
 	});
 	onDestroy(() => {
+		show = false;
 		console.log('component destroyed');
 	});
 </script>
@@ -422,21 +426,23 @@
 <div class="control-btn" class:show>
 	<!-- control emotional expressions -->
 	<div>
-		<Button on:click={handleClick} type="classic" flat={true}>neutral</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>happiness</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>disgust</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>surprise</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>anger</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>sadness</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>fear</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>worry</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>contempt</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>neutral</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>happiness</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>disgust</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>surprise</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>anger</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>sadness</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>fear</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>worry</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>contempt</Button>
 	</div>
 	<!-- control status-->
 	<div>
-		<Button on:click={handleClick} type="classic" flat={true}>wait</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>listen</Button>
-		<Button on:click={handleClick} type="classic" flat={true}>talk</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>wait</Button>
+		<Button on:click={changeExpression} type="classic" flat={true}>listen</Button>
+	</div>
+	<div>
+		<Button on:click={toggleTalkStatus} type="primary" flat={true}>talk</Button>
 	</div>
 </div>
 
