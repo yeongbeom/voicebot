9284ef3b141a3dc50576dcd83a6ebadc6fda6af4
comverser <hyunduk0206@gmail.com>
2022-03-30 04:50:49 +0000

    -

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-	import FontControls from '$lib/components/font-controls.svelte';
-	import EmotionalExpressions from '$lib/components/expressions/basic.svelte';
-
-
-
 </script>
 
 <div>
-	<a href="/test-expressions">Test expressions</a>
-
-	<FontControls />
+	<a href="/test/expressions">Test expressions</a>
+	<a href="/test/bubble">Test bubble</a>
 </div>
 
 <style>
