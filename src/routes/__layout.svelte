34646b5fd1ef69a3fa467411098de55da3ada417
Hyunduk Shin <hyunduk0206@gmail.com>
2022-04-06 10:59:40 +0900

    -

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../styles/global.css';
+</script>
+
+<svelte:head>
+	<title>KGC</title>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<div class="copyright">copyright 2022</div>
+</footer>
+
+<style>
+	footer {
+		padding: 40px;
+		text-align: center;
+	}
+	.copyright {
+		color: #aaa;
+		font-size: 14px;
+		display: inline-block;
+		padding: 20px;
+		border-top: 1px solid #ddd;
+	}
+</style>
