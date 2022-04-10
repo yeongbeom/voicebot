2b76b7b404d8956b8546a996246db0701de1c5e3
comverser <hyunduk0206@gmail.com>
2022-04-10 01:18:25 +0000

    -

--- src/routes/services/todo-list/todo-api/index.json.ts
+++ src/routes/services/todo-list/todo-api/index.json.ts
@@ -31,18 +31,3 @@ export const post: RequestHandler = async ({ request }) => {
 		body
 	};
 };
-
-// import type { RequestHandler, Request } from '@sveltejs/kit';
-// import { api } from './_api';
-
-// export const get: RequestHandler = (request) => {
-// 	return api(request);
-// };
-
-// export const post: RequestHandler<Request, FormData> = (request) => {
-// 	return api(request, {
-// 		created_at: new Date(),
-// 		text: request.body.get('text'),
-// 		done: false
-// 	});
-// };
