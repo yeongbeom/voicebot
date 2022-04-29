fee4f596865efce61f85d4f3bef70bee5c9ad08e
Hyunduk Shin <hyunduk0206@gmail.com>
2022-04-29 11:49:13 +0900

    -

---
+++ src/routes/expression-only/expression-only.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import VoiceBot from '$lib/components/voicebot.svelte';
+	import ControlFont from '$lib/components/control-font.svelte';
+	import { onMount } from 'svelte';
+	import { currentStatus, status } from '$lib/stores/bot';
+
+	onMount(() => {
+		$currentStatus = $status.idle;
+	});
+</script>
+
+<ControlFont />
+<VoiceBot />
