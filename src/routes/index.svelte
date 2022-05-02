66410679e7dc7045aed86dda45701669ff52738f
Hyunduk Shin <hyunduk0206@gmail.com>
2022-05-02 18:07:10 +0900

    -

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,10 @@
 	import VoiceBot from '$lib/components/VoiceBot.svelte';
 	import { onMount, onDestroy } from 'svelte';
 	import { currentStatus, status } from '$lib/stores/bot';
+	import { debugMode } from '$lib/stores/config';
 
 	onMount(() => {
+		$debugMode = false;
 		console.log('all.svelte mounted');
 		$currentStatus = $status.idle;
 	});
