525c936e3f58aaffb019ace37304b3c4ca6e9b70
Hyunduk Shin <hyunduk0206@gmail.com>
2022-05-09 10:13:38 +0900

    -

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import '../styles/global.css';
+
+	let isPageLoaded = false;
+
+	onMount(() => {
+		isPageLoaded = true;
+	});
 </script>
 
 <svelte:head>
@@ -25,11 +32,26 @@
 	</nav>
 </header>
 
+{#if !isPageLoaded}
+	<div class="loader">Loading...</div>
+{/if}
+
 <main>
 	<slot />
 </main>
 
 <style>
+	.loader {
+		position: fixed;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		display: grid;
+		place-items: center;
+		background-color: white;
+		z-index: 9999;
+	}
 	.links {
 		display: flex;
 		flex-wrap: wrap;
